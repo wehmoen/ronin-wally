ccd0468c4cc12e0598c1b6a697cebc4e29bfe03e
Nico <mail@wehmoen.dev>
2022-08-22 22:22:17 +0200

    Reuse reqwest client

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use dialoguer::Input;
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use reqwest_retry::policies::ExponentialBackoff;
+use reqwest_retry::RetryTransientMiddleware;
 use serde::{Deserialize, Serialize};
-use web3::types::{Address};
+use web3::types::Address;
 
 type RRTransactionHash = String;
 
@@ -15,7 +18,7 @@ struct RRTransaction {
     from: String,
     to: String,
     hash: String,
-    block_number: u64
+    block_number: u64,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -26,35 +29,42 @@ struct RRDecodedTransaction {
     hash: RRTransactionHash,
     block_number: u64,
     input: Option<serde_json::Value>,
-    output: Option<serde_json::Value>
+    output: Option<serde_json::Value>,
 }
 
-fn normalize_address(input: &String) -> String {
+fn normalize_address(input: &str) -> String {
     input.replace("ronin:", "0x")
 }
 
-#[derive(Serialize, Deserialize)]
 struct RoninRest {
     address: String,
+    host: String,
+    client: ClientWithMiddleware,
 }
 
 impl RoninRest {
     pub fn new(address: String) -> RoninRest {
         RoninRest {
             address,
+            host: "https://ronin.rest".into(),
+            client: ClientBuilder::new(reqwest::Client::new()).with(
+                RetryTransientMiddleware::new_with_policy(
+                    ExponentialBackoff::builder().build_with_max_retries(15)
+                )
+            ).build(),
         }
     }
 
     pub async fn sent_transactions(&self) -> RRTransactionDict {
         let data: RRTransactionDict = serde_json::from_str(
-            &reqwest::Client::new().get(format!("https://ronin.rest/archive/listSentTransactions/{}", self.address)).send().await.unwrap().text().await.unwrap()
+            &self.client.get(format!("{}/archive/listSentTransactions/{}", self.host, self.address)).send().await.unwrap().text().await.unwrap()
         ).unwrap();
 
         data
     }
     pub async fn received_transactions(&self) -> RRTransactionDict {
         let data: RRTransactionDict = serde_json::from_str(
-            &reqwest::Client::new().get(format!("https://ronin.rest/archive/listReceivedTransactions/{}", self.address)).send().await.unwrap().text().await.unwrap()
+            &self.client.get(format!("{}/archive/listReceivedTransactions/{}", self.host, self.address)).send().await.unwrap().text().await.unwrap()
         ).unwrap();
 
         data
@@ -62,7 +72,7 @@ impl RoninRest {
 
     pub async fn decode_method(&self, hash: &RRTransactionHash) -> serde_json::Value {
         let data: serde_json::Value = serde_json::from_str(
-            &reqwest::Client::new().get(format!("https://ronin.rest/ronin/decodeTransaction/{}", hash)).send().await.unwrap().text().await.unwrap()
+            &self.client.get(format!("{}/ronin/decodeTransaction/{}", self.host, hash)).send().await.unwrap().text().await.unwrap()
         ).unwrap();
 
         data
@@ -70,7 +80,7 @@ impl RoninRest {
 
     pub async fn decode_receipt(&self, hash: &RRTransactionHash) -> serde_json::Value {
         let data: serde_json::Value = serde_json::from_str(
-            &reqwest::Client::new().get(format!("https://ronin.rest/ronin/decodeTransactionReceipt/{}", hash)).send().await.unwrap().text().await.unwrap()
+            &self.client.get(format!("{}/ronin/decodeTransactionReceipt/{}", self.host, hash)).send().await.unwrap().text().await.unwrap()
         ).unwrap();
 
         data
@@ -78,13 +88,11 @@ impl RoninRest {
 
     pub async fn transaction(&self, hash: &RRTransactionHash) -> RRTransaction {
         let data: RRTransaction = serde_json::from_str(
-            &reqwest::Client::new().get(format!("https://ronin.rest/ronin/getTransaction/{}", hash)).send().await.unwrap().text().await.unwrap()
+            &self.client.get(format!("{}/ronin/getTransaction/{}", self.host, hash)).send().await.unwrap().text().await.unwrap()
         ).unwrap();
 
         data
     }
-
-
 }
 
 #[tokio::main]
@@ -124,8 +132,7 @@ async fn main() {
     let mut account_data: Vec<RRDecodedTransaction> = vec![];
 
     for hash in total {
-
-        let tx =rr.transaction(&hash).await;
+        let tx = rr.transaction(&hash).await;
 
         if tx.to != tx.from {
             account_data.push(
@@ -135,14 +142,14 @@ async fn main() {
                     output: Some(rr.decode_receipt(&hash).await),
                     hash: hash.clone(),
                     to: tx.to,
-                    block_number: tx.block_number
+                    block_number: tx.block_number,
                 }
             );
         }
         println!("Completed: {}", &hash);
     }
 
-    account_data.sort_by(|a,b| {
+    account_data.sort_by(|a, b| {
         a.block_number.cmp(&b.block_number)
     });
 
